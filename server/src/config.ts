376eb283cc65c64a0d29048d2cc8f11ad0df1440
Depardon <mickael.depardon@wizbii.com>
2020-12-22 11:44:48 +0100

    fix port

--- server/src/config.ts
+++ server/src/config.ts
@@ -10,7 +10,7 @@ interface ConfigType {
 }
 
 const config: ConfigType = {
-  port: Number(process.env.SERVER_PORT) || 8080,
+  port: Number(process.env.PORT) || Number(process.env.SERVER_PORT) || 8080,
   socketIoPort: Number(process.env.SOCKET_PORT) || 8081,
   uuidNameSpace: process.env.UUID_NAMESPACE || '00 02 32 ef 70 30 a8 ea e4 c5 21 69 95 da 8a c5',
   environment: process.env.ENVIRONMENT || 'PRODUCTION'
